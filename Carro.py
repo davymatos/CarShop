7797e61e4a0ff08907bb5713c5d36fed7bebea17
Davy de Matos <davymatos01@gmail.com>
2019-09-05 23:00:31 -0300

    Getters e Setters

--- Carro.py
+++ Carro.py
@@ -9,6 +9,38 @@ class Carro(Veiculo):
         self.ano = ano
         self.cor = cor
 
-c1 = Carro("Ford", "Ranger", 2019, "Branco")
-print("{} {} {} {}".format(c1.marca, c1.modelo, c1.ano, c1.cor))
+    @property
+    def getMarca(self):
+        return self.marca
+
+    def setMarca(self, marca):
+        self.marca = marca
+
+    @property
+    def getModelo(self):
+        return self.modelo
+
+    def setModelo(self, modelo):
+        self.modelo = modelo
+
+    @property
+    def getAno(self):
+        return self.ano
+
+    def setAno(self, ano):
+        self.ano = ano
+
+    @property
+    def getCor(self):
+        return self.cor
+
+    def setCor(self, cor):
+        self.cor = cor
+
+
+c1 = Carro()
+c1.marca = input("DIGITE O NOME DA MARCA: ")
+c1.modelo = input("DIGITE O NOME DA MODELO: ")
+c1.ano = input("DIGITE O NOME DA ANO: ")
+c1.cor = input("DIGITE O NOME DA COR: ")
 print(c1.marca, c1.modelo, c1.ano, c1.cor)
