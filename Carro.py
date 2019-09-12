7172113fa5a2a3f5e5bb03d8123926d001782942
Davy de Matos <38896707+SkwD1@users.noreply.github.com>
2019-09-12 17:40:50 -0300

    Modificação nos métodos e atributos

--- Carro.py
+++ Carro.py
@@ -1,60 +1,22 @@
-class Veiculo(object):
-    def __init__(self, chassis):
-        self.chassis = chassis
-
-    @property
-    def getChassis(self):
-        return self.chassis
-
-    def setChassis(self, chassis):
-        self.chassis = input("DIGITE O NOME DA CHASSIS: ")
+from Veiculo import Veiculo
 
 class Carro(Veiculo):
     def __init__(self, marca, modelo, ano, cor, chassis):
-        super(Carro, self).__init__(chassis)
-        self.marca = marca
-        self.modelo = modelo
-        self.ano = ano
-        self.cor = cor
+        super(Carro, self).__init__(marca, modelo, ano, cor, chassis)
         self.listaCar = []
 
     @property
-    def getMarca(self):
-        return self.marca
-
-    def setMarca(self, marca):
-        self.marca = input("DIGITE O NOME DA MARCA: ")
-
-    @property
-    def getModelo(self):
-        return self.modelo
-
-    def setModelo(self, modelo):
-        self.modelo = input("DIGITE O NOME DA MODELO: ")
-
-    @property
-    def getAno(self):
-        return self.ano
-
-    def setAno(self, ano):
-        self.ano = input("DIGITE O NOME DA ANO: ")
-
-    @property
-    def getCor(self):
-        return self.cor
-
-    def setCor(self, cor):
-        self.cor = input("DIGITE O NOME DA COR: ")
-
-    @property
     def getCarro(self):
-        return self.listaCar.append(Carro(c1.marca, c1.modelo, c1.ano, c1.cor, c1.chassis))
+        self.listaCar.append(c1.getMarca)
+        self.listaCar.append(c1.getModelo)
+        self.listaCar.append(c1.getAno)
+        self.listaCar.append(c1.getCor)
+        self.listaCar.append(c1.getChassis)
 
     def setCarro(self):
         self.listaCar.append(Carro)
 
-listaCar = []
-carros = []
+
 
 c1 = Carro(" ", " ", " ", " ", " ")
 c1.setMarca(c1.marca)
@@ -63,16 +25,14 @@ c1.setAno(c1.ano)
 c1.setCor(c1.cor)
 c1.setChassis(c1.chassis)
 
-'''
-listaCar.append(Carro(c1.marca, c1.modelo, c1.ano, c1.cor, c1.chassis))
+print("Carro: {} {}  |  Ano: {}  |  Cor: {}   |   Chassis: {}".format(c1.marca, c1.modelo, c1.ano, c1.cor, c1.chassis))
 
-for j in listaCar:
-    print(listaCar[0])
-    print(listaCar[1])
-    print(listaCar[2])
-    print(listaCar[3])
-    print(listaCar[4])
+print(c1.getCarro)
+
+
 '''
+listaCar = []
+carros = []
 
 listaCar.append(c1.getMarca)
 listaCar.append(c1.getModelo)
@@ -82,9 +42,11 @@ listaCar.append(c1.getChassis)
 
 carros.append(listaCar)
 
+
 for i in carros:
     print("Marca: ", i[0])
     print("Modelo: ", i[1])
     print("Ano: ", i[2])
     print("Cor: ", i[3])
     print("Chassis: ", i[4])
+'''
