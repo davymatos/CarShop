c18eff31db62e69460ea6db90a01b38c99f96e06
Davy de Matos <38896707+SkwD1@users.noreply.github.com>
2019-11-21 14:49:54 -0300

    Modificação no model

--- Carro.py
+++ Carro.py
@@ -16,36 +16,7 @@ class Carro(Veiculo):
         self.listaCar.append(c1.getCor())
         self.listaCar.append(c1.getChassis())
 
+    def exibirCarro(self):
+        return "Carro: {} {}  |  Ano: {}  |  Cor: {}   |   Chassis: {}".format(c1.marca, c1.modelo, c1.ano, c1.cor, c1.chassis)
 
 
-c1 = Carro(" ", " ", " ", " ", " ")
-c1.setMarca(c1.marca)
-c1.setModelo(c1.modelo)
-c1.setAno(c1.ano)
-c1.setCor(c1.cor)
-c1.setChassis(c1.chassis)
-
-print("Carro: {} {}  |  Ano: {}  |  Cor: {}   |   Chassis: {}".format(c1.marca, c1.modelo, c1.ano, c1.cor, c1.chassis))
-print(c1.getCarro())
-
-
-'''
-listaCar = []
-carros = []
-
-listaCar.append(c1.getMarca)
-listaCar.append(c1.getModelo)
-listaCar.append(c1.getAno)
-listaCar.append(c1.getCor)
-listaCar.append(c1.getChassis)
-
-carros.append(listaCar)
-
-
-for i in carros:
-    print("Marca: ", i[0])
-    print("Modelo: ", i[1])
-    print("Ano: ", i[2])
-    print("Cor: ", i[3])
-    print("Chassis: ", i[4])
-'''
